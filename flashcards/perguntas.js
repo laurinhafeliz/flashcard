79f339b5f8017d66edb9e90eeb68559f30a612a1
Laura Macedo <144124257+laurinhafeliz@users.noreply.github.com>
2024-09-30 07:57:43 -0300

    AULA 7

---
+++ flashcards/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Bioquímica',
+    'O que são enzimas??',
+    'Substâncias orgânicas com função catalisadora '
+)
+
+criaCartao(
+    'Anatomia',
+    'Quais as cinco divisões do corpo humano?',
+    'Cabeça, pescoço, tórax, abdome e pelve'
+)
+
+criaCartao(
+    'Fisiologia',
+    'Que nome recebe a célula especializada na propagação do impulso nervoso?',
+    'Neurônios'
+)
+
+criaCartao(
+    'Citologia',
+    'Os componentes celulares que estão presentes tanto em células de eucariontes como de procariontes são:',
+    'Membrana plasmática e ribossomos'
+)
